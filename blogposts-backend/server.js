5743d4e09d2daa703747f7c5b44a9395bc53f037
Thomas Cansino <166280962+thomascansino@users.noreply.github.com>
2024-10-24 23:58:57 +0800

    Update server.js

--- blogposts-backend/server.js
+++ blogposts-backend/server.js
@@ -8,10 +8,7 @@ const port = process.env.PORT || 5000;
 
 connectDb();
 
-app.use(cors({
-    origin: process.env.FRONTEND_URL, // allow requests from frontend server
-    credentials: true, // allows cookies or auth headers to be sent along with requests
-}));
+app.use(cors());
 
 app.use(express.json());
 
